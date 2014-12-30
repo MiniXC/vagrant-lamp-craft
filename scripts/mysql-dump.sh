e7fe835dc32062eddbf29ad4de87ad1d212169df
Christoph Minixhofer <minchc11@htlkaindorf.at>
2014-12-30 16:07:45 +0100

    remove blank line

--- scripts/mysql-dump.sh
+++ scripts/mysql-dump.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
 mysqldump -u root -p$2 $1 > /home/vagrant/mysql/dump.sql && echo "Backing up MySQL Database to dump.sql..."
--- scripts/mysql-restore.sh
+++ scripts/mysql-restore.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
 mysql -u root -p$2 $1 < /home/vagrant/mysql/dump.sql && echo "Restoring MySQL Database from dump.sql..."
